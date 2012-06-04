3376e4db76dcf80120a75e9c9cbc376b5b3b36fa
Brendon <425026+brendon-codes@users.noreply.github.com>
2012-06-03 23:27:48 -0700

    Adding mapper

--- 1/1.go
+++ 1/1.go
@@ -13,13 +13,36 @@ import "fmt"
 
 
 func main () () {
-    fmt.Printf("Sum: %d\n", sum([]int{5, 10}))
+    fmt.Printf("Mapper: %d\n", mapper(foo, []int{1, 3, 5}))
+    //fmt.Printf("Sum: %d\n", sum([]int{5, ;10}))
     //m := 10
     //incrs := []int{3, 5}
     //for _, incr := range incrs {
     //    fmt.Printf("Total: %d\n", cycle(0, m, incr))
     //}
     os.Exit(0)
+}
+
+
+func foo (i int) (int) {
+    return i + 2
+}
+
+
+func mapper (callback func(int) int, items[]int) ([]int) {
+    o := make([]int, len(items), len(items))
+    out_items := mapper_ints(items, o, 0, callback)
+    return out_items
+}
+
+
+func mapper_ints (in_items[]int, out_items[]int,
+                  i int, callback func(int) int) ([]int) {
+    if i < len(in_items) {
+        out_items[i] = callback(in_items[i])
+        return mapper_ints(in_items, out_items, i + 1, callback)
+    }
+    return out_items
 }
 
 
