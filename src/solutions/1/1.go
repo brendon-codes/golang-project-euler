3baa6e13f27239902716da258ac9a73752ea7e0f
Brendon <425026+brendon-codes@users.noreply.github.com>
2012-06-10 19:44:19 -0700

    int refactors

--- src/solutions/1/1.go
+++ src/solutions/1/1.go
@@ -14,10 +14,10 @@ import "fmt"
 
 func main () () {
     const m uint32 = 1000
-    var i uint32
-    var r map[uint32] bool = map[uint32] bool {}
-    var a uint32 = 0
     var incrs []uint32 = []uint32{3, 5}
+    var r map[uint32] bool = map[uint32] bool {}
+    var i uint32
+    var a uint32 = 0
     var v uint32
     for _, v = range incrs {
         for i = v; i < m; i += v {
