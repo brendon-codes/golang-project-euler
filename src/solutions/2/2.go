8a69c90a12c3e65123c61547344fbb7fe72f42c7
Brendon <425026+brendon-codes@users.noreply.github.com>
2012-06-10 18:14:55 -0700

    Solved problem 2

--- src/solutions/2/2.go
+++ src/solutions/2/2.go
@@ -13,7 +13,20 @@ import "fmt"
 
 
 func main () () {
-    fmt.Printf("%d\n", summed)
+    m := 4000000
+    v1 := 1
+    v2 := 2
+    v := v1
+    a := 0
+    for v <= m {
+        v = v1 + v2
+        v1 = v2
+        v2 = v
+        if v1 % 2 == 0 {
+            a += v1
+        }
+    }
+    fmt.Printf("%d\n", a)
     os.Exit(0)
 }
 
