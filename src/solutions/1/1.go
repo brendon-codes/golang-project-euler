ff3b9847c59c569213553483d7773ef57ef733bf
Brendon <425026+brendon-codes@users.noreply.github.com>
2012-06-10 17:35:34 -0700

    Solved #1

--- src/solutions/1/1.go
+++ src/solutions/1/1.go
@@ -13,56 +13,21 @@ import "fmt"
 
 
 func main () () {
+    var i int
+    r := map[int] bool {}
+    a := 0
     m := 1000
     incrs := []int{3, 5}
-    cycler := func (incr int) (int) {
-        return cycle(0, m, incr)
+    for _, v := range incrs {
+        for i = v; i < m; i += v {
+            if !r[i] {
+                a += i
+                r[i] = true
+            }
+        }
     }
-    mapped := mapper(cycler, incrs)
-    summed := sum(mapped)
-    fmt.Printf("%d\n", summed)
+    fmt.Printf("%d\n", a)
     os.Exit(0)
-}
-
-
-func mapper (callback func(int) int, items[]int) ([]int) {
-    o := make([]int, len(items), len(items))
-    out_items := mapper_ints(items, o, 0, callback)
-    return out_items
-}
-
-
-func mapper_ints (in_items[]int, out_items[]int,
-                  i int, callback func(int) int) ([]int) {
-    if i < len(in_items) {
-        out_items[i] = callback(in_items[i])
-        return mapper_ints(in_items, out_items, i + 1, callback)
-    }
-    return out_items
-}
-
-
-func sum (items[]int) (int) {
-    return sum_ints(items, 0, 0)
-}
-
-
-func sum_ints (items[]int, i int, t int) (int) {
-    if i < len(items) {
-        return sum_ints(items, i + 1, t + items[i])
-    }
-    return t
-}
-
-
-func cycle (i int, m int, incr int) (int) {
-    v := i + incr
-    if v < m {
-        return (incr + cycle(v, m, incr))
-    } else if v == m {
-        return 0
-    }
-    return i
 }
 
 
---
+++ src/solutions/2/2.go
@@ -0,0 +1,20 @@
+
+/**
+ * Project Euler
+ * Author: Brendon Crawford
+ *
+ * Problem: 2
+ */
+
+package main
+
+import "os"
+import "fmt"
+
+
+func main () () {
+    fmt.Printf("%d\n", summed)
+    os.Exit(0)
+}
+
+
